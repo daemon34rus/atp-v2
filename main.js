59de40e27e21b4cbe15b178e47b5180a19c777ea
jkcchan <jkcchan@uwaterloo.ca>
2015-05-21 21:25:52 -0400

    parallax

--- main.js
+++ main.js
@@ -18,14 +18,14 @@ $(document).ready(function(){
         // if yes, add “fixed” class to the <nav>
         // add padding top to the #content 
             (value is same as the height of the nav)
-            $('nav').addClass('fixed').css('top','0').next()
+            $('.nav').addClass('fixed').css('top','0').next()
             .css('padding-top','60px');
 
             } else {
 
         // when scroll up or less than aboveHeight,
             remove the “fixed” class, and the padding-top
-            $('nav').removeClass('fixed').next()
+            $('.nav').removeClass('fixed').next()
             .css('padding-top','0');
             }
         });
