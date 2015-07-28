8d2a657e800a61479c17bf6cd6e5f44d8a52d01f
jkcchan <jkcchan@uwaterloo.ca>
2015-07-28 01:48:31 -0400

    Wordpress migration!

--- main.js
+++ main.js
@@ -89,7 +89,7 @@
         "degrees":"BMSc",
         'position':'Founder, President of ATP',
         'info':"Janson is a 2013 Western University grad specializing in Biochemistry, and currently studying nursing at York University. Growing up with a brother with ASD has helped him understand the challenges and sacrifices similar families' have to make. As a nursing student representative, he has spoken and hosted multiple workshops discussing ATP at national Canadian nursing conferences.",
-        'imgsrc':"http://jkcchan.me/atp-v2/src/janson 2.jpg",
+        'imgsrc':"/public_html/src/janson 2.jpg",
         'linkedin':'https://www.linkedin.com/in/jansonchan'
       },
       {
@@ -98,7 +98,7 @@
         "degrees":"BSc",
         'position':'Co-founder & Vice President of Operations',
         'info':'Brandon is a recent graduate from Western University, with a B.Sc in Physiology and Psychology. Along with Janson, they pioneered Autism Awareness clubs at Western and York and has helped build ATP from its conception.',
-        'imgsrc':"http://jkcchan.me/atp-v2/src/brandon 2.jpg",
+        'imgsrc':"/public_html/src/brandon 2.jpg",
         'linkedin':'https://www.linkedin.com/pub/brandon-wildfong/a8/228/793?trk=pub-pbmap'
       },
       {
@@ -107,7 +107,7 @@
         "degrees":"BSc",
         'position':'Director of Public Relations',
         'info':'Jillian graduated from UofT in 2013 specializing in Biochemistry, and is currently studying nursing at York University. She grew up with a sister diagnosed with autism and witnessed her struggle with school and be judged by others. ATP has allowed her to fight this stigma and help others like her sister.',
-        'imgsrc':"http://jkcchan.me/atp-v2/src/jillian.jpg",
+        'imgsrc':"/public_html/src/jillian.jpg",
         'linkedin':'https://www.linkedin.com/pub/jillian-ferreira/b0/a82/639'
       },
       {
@@ -116,7 +116,7 @@
         "degrees":"BSc",
         'position':'Program Director ',
         'info':'Mamta is a recent UofT graduate with a specialist degree in Psychology, and currently completing a post-grad diploma in Behavioral Science Technology at George Brown. She has a younger sister with cerebral palsy inspiring her to be an advocate for vulnerable populations.',
-        'imgsrc':"http://jkcchan.me/atp-v2/src/mamta (2).jpg",
+        'imgsrc':"/public_html/src/mamta (2).jpg",
         'linkedin':'http://ca.linkedin.com/pub/mamta-prajapati/93/a94/567'
       },
       {
@@ -125,7 +125,7 @@
         "degrees":"BSc",
         'position':'Director of Human Resources',
         'info':'Cindy graduated with her Bachelor of Science in Nursing from UOIT. She has maintained an active and passionate role as a volunteer within our program, earning her the title of Director of Human Resources.',
-        'imgsrc':"http://jkcchan.me/atp-v2/src/cindy.jpg",
+        'imgsrc':"/public_html/src/cindy.jpg",
         'linkedin':''
       },
       {
@@ -134,7 +134,7 @@
         "degrees":"BSc",
         'position':'Director of Finance',
         'info':"Shehan graduated from Queen’s University with his B.Sc in Genetics.  He is one of our behind the scenes member but actively engages with our participants and program every week!",
-        'imgsrc':"http://jkcchan.me/atp-v2/src/shehan.jpg",
+        'imgsrc':"/public_html/src/shehan.jpg",
         'linkedin':''
       }];
     });
